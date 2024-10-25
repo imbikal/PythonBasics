6e85c0a8090320f6c85e89748f63650f25b086ed
bikal <106312691+imbikal@users.noreply.github.com>
2024-10-25 14:18:52 +0545

    Chapter-2

---
+++ 03_rules_variables.py
@@ -0,0 +1,19 @@
+# Rules for Variables in Python, valid or invalid :
+"""
+1. A variable name can contain alphabets,digits and underscores
+2. A variable name can only start with an alphabet and underscores.
+3. A variable name can't start with digit.
+4. No white space allowed to be used inside variable name
+"""
+a = 23
+
+b = 443223
+
+python = 24414
+
+_python = 19334
+
+#  @python = 645 
+ 
+""" Here @ in starting is not allowed because it doesn't start
+ with alphabet and underscores i.e Special character not allowed """
